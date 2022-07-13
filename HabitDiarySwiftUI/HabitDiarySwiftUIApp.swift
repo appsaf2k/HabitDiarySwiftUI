bc94bf0f889134fe6ffdc960d4eadf5a09d8fcc8
appsaf2k <appsaf2k@gmail.com>
2022-07-13 10:23:21 +0300

    Initial Commit

---
+++ HabitDiarySwiftUI/HabitDiarySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitDiarySwiftUIApp.swift
+//  HabitDiarySwiftUI
+//
+//  Created by @andreev2k on 13.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitDiarySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
